ca706ac23943ab6c42fda920f72ccf9c25dc331e
a <102840087+kingdudely@users.noreply.github.com>
2025-10-14 09:18:33 -0400

    Delete compl/src/Compiler.cpp

--- compl/src/Compiler.cpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include "luacode.h"
-
-int main() {
-    std::string src = "print'Hello World'";
-    size_t bcs;
-    luau_compile(src.c_str(), src.size(), nullptr, &bcs);
-    return 0;
-}
