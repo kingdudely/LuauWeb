e8b45457a92eb7a6f057d5c81da7787b948e1fef
a <102840087+kingdudely@users.noreply.github.com>
2025-10-15 11:35:39 -0400

    Update main.cpp

--- compl/src/main.cpp
+++ compl/src/main.cpp
@@ -1,15 +1,1 @@
-#include <string>
-#include "luacode.h"
-#include "lua.h"
-#include "lualib.h"
 
-int main() {
-    std::string src = ""; // print'Hello World'
-    size_t bcs;
-    luau_compile(src.c_str(), src.size(), nullptr, &bcs);
-
-    lua_State* l = luaL_newstate();
-    lua_close(l);
-
-    return 0;
-}
