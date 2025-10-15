e4d73946fc352afdd18993fecf1b97b2666119f0
a <102840087+kingdudely@users.noreply.github.com>
2025-10-15 11:45:45 -0400

    Create fullbuild.sh

---
+++ compl/fullbuild.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+git clone https://github.com/emscripten-core/emsdk.git $HOME/emsdk
+cd $HOME/emsdk
+./emsdk install latest
+./emsdk activate latest
+
+source $HOME/emsdk/emsdk_env.sh
+
+rm -rf ./luau
+git clone https://github.com/Roblox/luau.git ./luau
+
+rm -rf ./build
+mkdir build
+cd build
+emcmake cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake --build . --target LuauWeb
